0db4616fd1f1b2e74ecb2df85d777c601131542e
ak-z-world <aryutechnologies2024@gmail.com>
2026-02-25 11:50:42 +0530

    efesrdfg

--- Aryu/Aryu/settings.py
+++ Aryu/Aryu/settings.py
@@ -285,10 +285,10 @@ TWILIO_AUTH_TOKEN = "44fbdfc9f0960b464c20a193b797c7f7"
 TWILIO_PHONE_NUMBER = "+15075854260"
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_BASE_URL = "https://aylms.aryuprojects.com/api"
+MEDIA_BASE_URL = "https://portal.aryuacademy.com/api"
 
 DATABASES = {
     'default': {
