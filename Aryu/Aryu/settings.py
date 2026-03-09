fd0da0003a143fb5d7a3590613a5fa1a558cf85d
ak-z-world <aryutechnologies2024@gmail.com>
2026-03-09 10:38:03 +0530

    roles

--- Aryu/Aryu/settings.py
+++ Aryu/Aryu/settings.py
@@ -196,13 +196,13 @@ CRONJOBS = [
 CORS_ALLOWED_ORIGINS = [
     "https://portal.aryuacademy.com",
     "https://workshop.aryuacademy.com",
-    "http://localhost:3000"
+    # "http://localhost:3000"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://portal.aryuacademy.com",
     "https://workshop.aryuacademy.com",
-    "http://localhost:3000"
+    # "http://localhost:3000"
 ]
 
 ALLOWED_HOSTS = [
