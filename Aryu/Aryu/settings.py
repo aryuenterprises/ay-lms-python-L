e3557de783785800759d3ac0e6568fbb0097cdb7
ak-z-world <aryutechnologies2024@gmail.com>
2026-03-17 10:14:37 +0530

    throttle remo

--- Aryu/Aryu/settings.py
+++ Aryu/Aryu/settings.py
@@ -159,9 +159,6 @@ REST_FRAMEWORK = {
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle"
     ],
-    "DEFAULT_THROTTLE_RATES": {
-        "anon": "10/min"
-    }
 }
 
 
