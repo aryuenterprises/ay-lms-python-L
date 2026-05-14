7dc77798af26df127515373f2afd5396c4a17790
ak-z-world <aryutechnologies2024@gmail.com>
2026-05-14 16:44:01 +0530

    sdf

--- Aryu/payments/serializers.py
+++ Aryu/payments/serializers.py
@@ -78,7 +78,7 @@ class PaymentTransactionDetailSerializer(serializers.ModelSerializer):
 
         if obj.invoice and hasattr(obj.invoice, "url"):
             return (
-                "https://aylms.aryuprojects.com/api"
+                "https://portal.aryuacademy.com/api"
                 + obj.invoice.url
             )
 
@@ -348,7 +348,7 @@ class PaymentTransactionCreateSerializer(serializers.ModelSerializer):
 
         if obj.invoice and hasattr(obj.invoice, "url"):
             return (
-                "https://aylms.aryuprojects.com/api"
+                "https://portal.aryuacademy.com/api"
                 + obj.invoice.url
             )
 
