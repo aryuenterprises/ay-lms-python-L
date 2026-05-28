ecb5311864043abfb33c14a37216f9b4860e28e4
Tamil-coder <tamilselvi12022004@gmail.com>
2026-05-28 14:42:52 +0530

    resources

--- Aryu/resources/urls.py
+++ Aryu/resources/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     #Resources urls
     path('resources',ResourcesViewset.as_view({'post':'create','get':'list'})),
     path('resources/<int:pk>',ResourcesViewset.as_view({'patch':'partial_update','delete':'destroy'})),
+    #FormsUrls
+    # path('form',FormViewset.as_view({'post':'create','get':'list','delete':'destroy'})),
 ]
