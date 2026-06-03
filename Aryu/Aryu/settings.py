96b7a0a12944d28c5664c6361d7e1fd891afd0e3
Tamil-coder <tamilselvi12022004@gmail.com>
2026-06-03 15:08:11 +0530

    lochal

--- Aryu/Aryu/settings.py
+++ Aryu/Aryu/settings.py
@@ -216,7 +216,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://workshop.aryuacademy.com",
     "https://aryuacademy.com",
     "https://passats.aryuacademy.com",
-    # "http://127.0.0.1:8000",
+    "http://127.0.0.1:8000",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -224,7 +224,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://workshop.aryuacademy.com",
     "https://aryuacademy.com",
     "https://passats.aryuacademy.com",
-    # "http://127.0.0.1:8000",
+    "http://127.0.0.1:8000",
 ]
 
 ALLOWED_HOSTS = [
@@ -232,8 +232,8 @@ ALLOWED_HOSTS = [
     "workshop.aryuacademy.com",
     "aryuacademy.com",
     "passats.aryuacademy.com",
-    # "localhost",
-    # "127.0.0.1",
+    "localhost",
+    "127.0.0.1",
 ]
 
 
