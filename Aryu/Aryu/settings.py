72e5ddb58a839bc6fd6fbfa219f8d23f0f2c329d
Tamil-coder <tamilselvi12022004@gmail.com>
2026-06-03 14:55:22 +0530

    settings

--- Aryu/Aryu/settings.py
+++ Aryu/Aryu/settings.py
@@ -215,7 +215,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://portal.aryuacademy.com",
     "https://workshop.aryuacademy.com",
     "https://aryuacademy.com",
-    "https://passats.aryuacademy.com/",
+    "https://passats.aryuacademy.com",
     # "http://127.0.0.1:8000",
 ]
 
@@ -223,7 +223,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://portal.aryuacademy.com",
     "https://workshop.aryuacademy.com",
     "https://aryuacademy.com",
-    "https://passats.aryuacademy.com/",
+    "https://passats.aryuacademy.com",
     # "http://127.0.0.1:8000",
 ]
 
