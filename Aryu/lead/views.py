08c827fb0b849dc3f8081fc327271f599a1810e6
ak-z-world <aryutechnologies2024@gmail.com>
2026-06-16 14:21:13 +0530

    updates

--- Aryu/lead/views.py
+++ Aryu/lead/views.py
@@ -399,7 +399,7 @@ class LeadViewSet(
                     "call_type": log.call_type,
                     "duration_seconds": log.duration_seconds,
                     "recording": (
-                        "https://aylms.aryuprojects.com/api" + log.recording_url.url
+                        "https://portal.aryuacademy.com/api" + log.recording_url.url
                         if log.recording_url and hasattr(log.recording_url, "url")
                         else None
                     ),
