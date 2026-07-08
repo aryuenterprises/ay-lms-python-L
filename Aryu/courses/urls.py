a5521c182e6b1bdee3eed03f21aca85f9f53385d
Tamil-coder <tamilselvi12022004@gmail.com>
2026-07-08 17:44:08 +0530

    syllabus

--- Aryu/courses/urls.py
+++ Aryu/courses/urls.py
@@ -4,10 +4,10 @@ from .views import *
 urlpatterns = [
    path('courses', CourseViewSet.as_view({'get': 'list', 'post': 'create', 'put': 'update', 'patch': 'partial_update'})),
    path('courses/<str:course_id>', CourseViewSet.as_view({'get': 'retrieve','put': 'update','patch': 'partial_update','delete': 'destroy'})),
-   path('courses/<str:course_id>/batches', CourseViewSet.as_view({'get': 'get_batches'})),
-   path('courses/<str:course_id>/archive', CourseViewSet.as_view({'patch': 'archive_course'})),
    path('courses/<str:course_id>/video',CourseVideoViewSet.as_view({'get':'list','post':'create'})),
    path('courses/<str:course_id>/video/<int:video_id>',CourseVideoViewSet.as_view({'delete':'destroy','patch':'update'})),
+   path('courses/<str:course_id>/batches', CourseViewSet.as_view({'get': 'get_batches'})),
+   path('courses/<str:course_id>/archive', CourseViewSet.as_view({'patch': 'archive_course'})),
    path('course_categories', CourseCategoryViewSet.as_view({'get': 'list', 'post': 'create'})),
    path('course_categories/<str:category_id>', CourseCategoryViewSet.as_view({'get': 'retrieve','put': 'update','patch': 'partial_update',})),
    path('course_categories/<str:category_id>/archive', CourseCategoryViewSet.as_view({'patch': 'archive_category'})),
@@ -15,7 +15,8 @@ urlpatterns = [
    path('courses/<str:course_id>/topic/<str:student_id>/status', StudentTopicStatusViewSet.as_view({ 'get': 'list' ,'post': 'create'}), name='topic-status'),
    path('courses/<str:course_id>/topic/<int:pk>', TopicViewSet.as_view({'get': 'retrieve','put': 'update','patch': 'partial_update',}), name='topic-detail'),
    path('courses/<str:course_id>/topic/<int:pk>/archive',TopicViewSet.as_view({'delete': 'destroy',})),
+   path('courses/<str:course_id>/syllabus',CourseSyllabusViewSet.as_view({'get': 'list', 'post': 'create'})),
    path('courses/<str:course_id>/syllabus/<int:syllabus_id>',CourseSyllabusViewSet.as_view({'delete': 'destroy', 'patch': 'update'})),
-]
 
-   
+]
+    
