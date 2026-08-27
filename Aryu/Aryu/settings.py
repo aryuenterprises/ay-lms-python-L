4c7341095b4f9f47713b07f1a6e9c8dc3e2d3f31
Tamil-coder <tamilselvi12022004@gmail.com>
2026-08-27 16:25:59 +0530

    settings db changed to live

--- Aryu/Aryu/settings.py
+++ Aryu/Aryu/settings.py
@@ -651,56 +651,6 @@ MEDIA_BASE_URL = "https://aylms.aryuprojects.com/api"
 #         },
 #     }
 
-class DisableMigrations:
-    def __getitem__(self, item):
-        return None
-    def __contains__(self, item):
-        return True
-
-if 'test' in sys.argv:
-    import django.contrib.postgres.fields
-    import django.db.models
-    from django.db.backends.sqlite3.schema import DatabaseSchemaEditor
-
-    class DummyArrayField(django.db.models.JSONField):
-        def __init__(self, *args, **kwargs):
-            kwargs.pop('base_field', None)
-            kwargs.pop('size', None)
-            super().__init__(*args, **kwargs)
-
-    django.contrib.postgres.fields.ArrayField = DummyArrayField
-
-    orig_quote_name = DatabaseSchemaEditor.quote_name
-    def safe_quote_name(self, name):
-        if "." in name:
-            name = name.split(".")[-1].strip('"')
-        return orig_quote_name(self, name)
-    DatabaseSchemaEditor.quote_name = safe_quote_name
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-        }
-    }
-    MIGRATION_MODULES = DisableMigrations()
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'aylms_live',  
-            'USER': 'aylms_live',
-            'PASSWORD':'KfdW543FDdfg',
-            'HOST': '187.127.178.144',   
-            'PORT': '5432',  
-            'AUTOCOMMIT': True,
-            'CONN_MAX_AGE': 60,
-            'OPTIONS': {
-                'options': '-c search_path=livequiz,public'
-            }
-        },
-    }
-
 # class DisableMigrations:
 #     def __getitem__(self, item):
 #         return None
@@ -738,11 +688,11 @@ else:
 #     DATABASES = {
 #         'default': {
 #             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'aylms_staging',
-#             'USER': 'aryu_user',
-#             'PASSWORD':'YUra@2025',
-#             'HOST': '49.207.178.161',
-#             'PORT': '5432',
+#             'NAME': 'aylms_live',  
+#             'USER': 'aylms_live',
+#             'PASSWORD':'KfdW543FDdfg',
+#             'HOST': '187.127.178.144',   
+#             'PORT': '5432',  
 #             'AUTOCOMMIT': True,
 #             'CONN_MAX_AGE': 60,
 #             'OPTIONS': {
@@ -750,6 +700,56 @@ else:
 #             }
 #         },
 #     }
+
+class DisableMigrations:
+    def __getitem__(self, item):
+        return None
+    def __contains__(self, item):
+        return True
+
+if 'test' in sys.argv:
+    import django.contrib.postgres.fields
+    import django.db.models
+    from django.db.backends.sqlite3.schema import DatabaseSchemaEditor
+
+    class DummyArrayField(django.db.models.JSONField):
+        def __init__(self, *args, **kwargs):
+            kwargs.pop('base_field', None)
+            kwargs.pop('size', None)
+            super().__init__(*args, **kwargs)
+
+    django.contrib.postgres.fields.ArrayField = DummyArrayField
+
+    orig_quote_name = DatabaseSchemaEditor.quote_name
+    def safe_quote_name(self, name):
+        if "." in name:
+            name = name.split(".")[-1].strip('"')
+        return orig_quote_name(self, name)
+    DatabaseSchemaEditor.quote_name = safe_quote_name
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
+    MIGRATION_MODULES = DisableMigrations()
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'aylms_staging',
+            'USER': 'aryu_user',
+            'PASSWORD':'YUra@2025',
+            'HOST': '49.207.178.161',
+            'PORT': '5432',
+            'AUTOCOMMIT': True,
+            'CONN_MAX_AGE': 60,
+            'OPTIONS': {
+                'options': '-c search_path=livequiz,public'
+            }
+        },
+    }
 
 # DATABASES = {
 #     'default': {
